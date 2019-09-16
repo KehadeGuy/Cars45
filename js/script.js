8e4954415abc72bef4f64b015c4857ce42d459f0
Kehade <kehade91@yahoo.com>
2019-09-16 14:35:49 +0100

    work

--- js/script.js
+++ js/script.js
@@ -6,3 +6,14 @@ jQuery(window).load(function() {
 	$('.carousel').carousel('pause');
 	
 });
+function myChange(a) {
+	console.log(a);
+	document.getElementsByClassName('changing')[0].style.display = 'none';
+	document.getElementsByClassName('changing')[1].style.display = 'none';
+	document.getElementsByClassName('changing')[2].style.display = 'none';
+	document.getElementsByClassName('changing')[3].style.display = 'none';
+
+	document.getElementsByClassName('changing')[a].style.display = 'flex';
+	document.getElementsByClassName('changing')[a].classList.remove('d-none');
+
+}
